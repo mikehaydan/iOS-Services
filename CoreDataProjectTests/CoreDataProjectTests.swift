73fc313d940787f74c616a2785fa32400afeb69d
Mike Haydan <mike.haydan.git@gmail.com>
2025-10-01 17:10:09 +0200

    Initial Commit

---
+++ CoreDataProjectTests/CoreDataProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectTests.swift
+//  CoreDataProjectTests
+//
+//  Created by Mykhailo Haidan on 01/10/2025.
+//
+
+import Testing
+@testable import CoreDataProject
+
+struct CoreDataProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
